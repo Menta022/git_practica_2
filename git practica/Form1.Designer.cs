20d57f9d9900354c26738d3e7ebe82efc0db1ab8
Menta0222 <haseo54@hotmail.com>
2024-02-16 15:34:28 -0300

    fondo verde

--- git practica/Form1.Designer.cs
+++ git practica/Form1.Designer.cs
@@ -200,7 +200,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackColor = System.Drawing.Color.Green;
             this.ClientSize = new System.Drawing.Size(739, 412);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label6);
