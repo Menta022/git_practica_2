5c946fbfa86ecbbcb987c1d6c1af567f83dd0d9f
Menta0222 <haseo54@hotmail.com>
2024-02-16 17:12:15 -0300

    dfasdfsdf

--- git practica/Form1.Designer.cs
+++ git practica/Form1.Designer.cs
@@ -228,6 +228,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 15;
+            this.textBox1.Text = "prueba main-2";
             // 
             // textBox2
             // 
